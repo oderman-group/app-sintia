eb74fc950f91dc7e4f17b1e4f05e5ab75fc18870
ing-enuarlara <enuar2110@gmail.com>
2025-01-19 21:22:45 +0100

    PES-1183 - Se crea archivo de chat para los docentes

---
+++ main-app/docente/chat2.php
@@ -0,0 +1,66 @@
+<?php include("session.php"); ?>
+<?php $idPaginaInterna = 'DC0148'; ?>
+<?php include("../compartido/historial-acciones-guardar.php"); ?>
+<?php include("../compartido/head.php"); ?>
+
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+<link href="../../config-general/assets/css/chat2.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+<style>
+    .contenedor2 {
+
+        position: relative;
+        padding: 10px;
+    }
+
+    .div-interior2 {
+        position: absolute;
+        bottom: -40px;
+        left: 40px;
+        /* Establece el color del elemento i interior */
+    }
+
+    .esquina-superior {
+        position: absolute;
+        top: 15px;
+        left: 10px;
+        transform: translateX(-50%);
+        /* Establece el color del span en la esquina superior izquierda */
+    }
+</style>
+
+</head>
+<!-- END HEAD -->
+<?php include("../compartido/body.php"); ?>
+<div class="page-wrapper">
+    <?php include("../compartido/encabezado.php"); ?>
+    <?php include("../compartido/panel-color.php"); ?>
+    <!-- start page container -->
+    <?php include("../compartido/chat-contenido.php"); ?>
+    <!-- end page container -->
+    <?php include("../compartido/footer.php"); ?>
+</div>
+
+
+<!-- start js include path -->
+<script src="../../config-general/assets/plugins/jquery/jquery.min.js"></script>
+<script src="../../config-general/assets/plugins/popper/popper.js"></script>
+<script src="../../config-general/assets/plugins/jquery-blockui/jquery.blockui.min.js"></script>
+<script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+<!-- bootstrap -->
+<script src="../../config-general/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../config-general/assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<!-- Common js-->
+<script src="../../config-general/assets/js/app.js"></script>
+<script src="../../config-general/assets/js/layout.js"></script>
+<script src="../../config-general/assets/js/theme-color.js"></script>
+<!-- notifications -->
+<script src="../../config-general/assets/plugins/jquery-toast/dist/jquery.toast.min.js"></script>
+<script src="../../config-general/assets/plugins/jquery-toast/dist/toast.js"></script>
+<!-- Material -->
+<script src="../../config-general/assets/plugins/material/material.min.js"></script>
+<!-- end js include path -->
+
+</body>
+
+</html>
