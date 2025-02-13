0dca0a06de85c6b6349b704f05308f8b739c68b6
luigiecalo02 <114239695+luigiecalo02@users.noreply.github.com>
2025-02-13 16:32:59 -0500

    Update index.php

--- main-app/index.php
+++ main-app/index.php
@@ -73,7 +73,7 @@ if(!isset($_GET['nodb'])) {
                                 </button>
                                 <label for="password">Contraseña</label>
                                 <div class="invalid-feedback">Por favor ingresa tu contraseña para continuar******</div>
-                                <div class="form-text" id="caps-lock-message" style="display: none;">Mayúsculas activadas</div>
+                                <div class="form-text" id="caps-lock-message" style="display: none;">Mayúsculas activadas </div>
                             </div><br>
 
                             <?php
