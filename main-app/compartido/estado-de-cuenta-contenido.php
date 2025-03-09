f5a7f2ddc81ba5f5b25aac3eda2f0d50b251d9db
ing-enuarlara <enuar2110@gmail.com>
2025-03-09 19:14:37 +0100

    HTF - Se corrige informacion en el estado de cuenta de los usuarios

--- main-app/compartido/estado-de-cuenta-contenido.php
+++ main-app/compartido/estado-de-cuenta-contenido.php
@@ -1,3 +1,4 @@
+<?php require_once(ROOT_PATH."/main-app/class/Movimientos.php"); ?>
 					<div class="row">
                         <div class="col-md-12">
                             <div class="row">
@@ -6,12 +7,15 @@
 								(SELECT sum(fcu_valor) FROM ".BD_FINANCIERA.".finanzas_cuentas WHERE fcu_usuario='".$_SESSION["id"]."' AND fcu_anulado=0 AND fcu_tipo=1 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}),
 								(SELECT sum(fcu_valor) FROM ".BD_FINANCIERA.".finanzas_cuentas WHERE fcu_usuario='".$_SESSION["id"]."' AND fcu_anulado=0 AND fcu_tipo=2 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}),
 								(SELECT sum(fcu_valor) FROM ".BD_FINANCIERA.".finanzas_cuentas WHERE fcu_usuario='".$_SESSION["id"]."' AND fcu_anulado=0 AND fcu_tipo=3 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}),
-								(SELECT sum(fcu_valor) FROM ".BD_FINANCIERA.".finanzas_cuentas WHERE fcu_usuario='".$_SESSION["id"]."' AND fcu_anulado=0 AND fcu_tipo=4 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]})
+								(SELECT sum(pi.payment) FROM ".BD_FINANCIERA.".payments p
+								INNER JOIN ".BD_FINANCIERA.".payments_invoiced pi ON pi.payments=p.cod_payment
+								WHERE p.invoiced='".$_SESSION["id"]."' AND p.institucion={$config['conf_id_institucion']} AND p.year={$_SESSION["bd"]})
 								"), MYSQLI_BOTH);
-								$saldo = ($resumen[0] - $resumen[2]);
+								$saldo = ($resumen[3] - $resumen[0]);
+								$colorSaldo = 'black';
 								$mensajeSaldo=$frases[309][$datosUsuarioActual['uss_idioma']];
-								if($saldo>0){$mensajeSaldo=$frases[310][$datosUsuarioActual['uss_idioma']];}
-								if($saldo<0){$mensajeSaldo=$frases[311][$datosUsuarioActual['uss_idioma']];}
+								if($saldo>0){$mensajeSaldo=$frases[310][$datosUsuarioActual['uss_idioma']]; $colorSaldo = 'green';}
+								if($saldo<0){$mensajeSaldo=$frases[311][$datosUsuarioActual['uss_idioma']]; $colorSaldo = 'red';}
 								?>
 								<div class="col-md-3">
 									
@@ -19,17 +23,17 @@
 										<header class="panel-heading panel-heading-purple"><?=strtoupper($frases[312][$datosUsuarioActual['uss_idioma']]);?> </header>
                                         <div class="panel-body">
 											<?php
-											if(empty($resumen[2])){
-												$resumen[2]=0;
+											if(empty($resumen[4])){
+												$resumen[4]=0;
 											}
-											if(empty($resumen[0])){
-												$resumen[0]=0;
+											if(empty($resumen[1])){
+												$resumen[1]=0;
 											}
 											?>
-											<p><b><?=strtoupper($frases[313][$datosUsuarioActual['uss_idioma']]);?>:</b> $<?=number_format($resumen[2],0,",",".");?></p>
-											<p><b><?=strtoupper($frases[314][$datosUsuarioActual['uss_idioma']]);?>:</b> $<?=number_format($resumen[0],0,",",".");?></p>
+											<p><b><?=strtoupper($frases[313][$datosUsuarioActual['uss_idioma']]);?>:</b> $<?=number_format($resumen[0],0,",",".");?></p>
+											<p><b><?=strtoupper($frases[413][$datosUsuarioActual['uss_idioma']]);?>:</b> $<?=number_format($resumen[3],0,",",".");?></p>
 											<hr>
-											<p><b><?=strtoupper($frases[315][$datosUsuarioActual['uss_idioma']]);?>:</b> $<?=number_format($saldo,0,",",".");?></p>
+											<p><b><?=strtoupper($frases[315][$datosUsuarioActual['uss_idioma']]);?>:</b> <span style="color: <?=$colorSaldo;?>;">$<?=number_format($saldo,0,",",".");?></span></p>
 											<p style="color: blueviolet;"><?=$mensajeSaldo;?></p>
 										</div>
 									</div>
@@ -64,30 +68,39 @@
                                                         <th>#</th>
 														<th><?=$frases[51][$datosUsuarioActual['uss_idioma']];?></th>
 														<th><?=$frases[162][$datosUsuarioActual['uss_idioma']];?></th>
-														<th><?=$frases[53][$datosUsuarioActual['uss_idioma']];?></th>
-														<th><?=$frases[52][$datosUsuarioActual['uss_idioma']];?></th>
+														<th><?= $frases[107][$datosUsuarioActual['uss_idioma']]; ?></th>
+														<th><?= $frases[413][$datosUsuarioActual['uss_idioma']]; ?></th>
+														<th><?= $frases[418][$datosUsuarioActual['uss_idioma']]; ?></th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
 													<?php
-													 $consulta = mysqli_query($conexion, "SELECT * FROM ".BD_FINANCIERA.".finanzas_cuentas 
-													 WHERE fcu_usuario='".$_SESSION["id"]."' AND fcu_anulado=0 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
-													 $contReg = 1;
-													 while($resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH)){
-														 $colorValor = 'black';
-														 if($resultado['fcu_tipo']==3) $colorValor = 'red';
-													 ?>
+														$consulta = mysqli_query($conexion, "SELECT * FROM " . BD_FINANCIERA . ".finanzas_cuentas fc
+														WHERE fcu_usuario='{$_SESSION["id"]}' AND fcu_anulado=0
+															AND fc.institucion={$_SESSION['idInstitucion']} 
+															AND fc.year='{$_SESSION["bd"]}' 
+														ORDER BY fc.id_nuevo DESC");
+														$contReg = 1;
+														while($resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH)){
+															$vlrAdicional = !empty($resultado['fcu_valor']) ? $resultado['fcu_valor'] : 0;
+															$totalNeto    = Movimientos::calcularTotalNeto($conexion, $config, $resultado['fcu_id'], $vlrAdicional);
+															$abonos       = Movimientos::calcularTotalAbonado($conexion, $config, $resultado['fcu_id']);
+															$porCobrar    = $totalNeto - $abonos;
+
+															$colorValor = $porCobrar > 0 ? 'red' : 'black';
+													?>
 													<tr>
                                                         <td><?=$contReg;?></td>
 														<td><?=$resultado['fcu_fecha'];?></td>
 														<td><?=$resultado['fcu_detalle'];?></td>
-														<td><?=$tipoEstadoFinanzas[$resultado['fcu_tipo']];?></td>
-														<td style="color:<?=$colorValor;?>;">$<?=number_format($resultado['fcu_valor'],0,",",".");?></td>
+														<td id="totalNeto<?= $resultado['fcu_id']; ?>" data-tipo="<?= $resultado['fcu_tipo'] ?>" data-total-neto="<?= $totalNeto ?>">$<?= !empty($totalNeto) ? number_format($totalNeto, 0, ",", ".") : 0 ?></td>
+														<td data-abonos="<?= $abonos ?>">$<?= !empty($abonos) ? number_format($abonos, 0, ",", ".") : 0 ?></td>
+														<td style="color:<?=$colorValor;?>;" data-por-cobrar="<?= $porCobrar ?>">$<?= !empty($porCobrar) ? number_format($porCobrar, 0, ",", ".") : 0 ?></td>
                                                     </tr>
 													<?php 
-														 $contReg++;
-													  }
-													  ?>
+															$contReg++;
+														}
+													?>
                                                 </tbody>
                                             </table>
                                             </div>
