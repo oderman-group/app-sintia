a19915166995e50287de77e7ffed7d7813569c5e
Jorge Diaz <ing.jorgediaz@outlook.com>
2025-06-06 11:06:18 -0500

    se agrega metodo para obtener los usuarios que son directivos y la suscripcion

--- main-app/class/App/Comunicativo/Usuarios_Notificaciones.php
+++ main-app/class/App/Comunicativo/Usuarios_Notificaciones.php
@@ -12,4 +12,21 @@ class Comunicativo_Usuarios_Notificaciones extends BDT_Tablas {
 
     CONST TIPO_NOTIFICACION_DESBLOQUEO_USUARIO = 1;
 
+    public static function ObtenerUsuariosDirectivosSuscripcion($year,$institucion)
+    {  
+        $sql = 'SELECT 
+                us.uss_id,us.uss_usuario,us.uss_email,us.uss_nombre, us.uss_bloqueado,us.uss_estado, IFNULL(un.upn_id, 0) upn_id
+                FROM mobiliar_general.usuarios us
+                LEFT JOIN mobiliar_general.usuarios_notificaciones un ON 
+                    un.upn_tipo_notificacion = 1 AND 
+                    un.upn_usuario = us.uss_id AND 
+                    un.institucion = us.institucion AND 
+                    un.`year` = us.`year`
+                WHERE us.uss_tipo = 5 AND us.institucion = '.$institucion.' AND us.`year` = '.$year.';
+            ';
+
+        return self::ejecutarSQL($sql);
+
+    }
+
 }
