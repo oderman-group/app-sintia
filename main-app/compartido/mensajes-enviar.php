03c765e413acbbcfc2b6ad060714848a07f9545e
El gran Sherman <jhooderman@gmail.com>
2025-06-08 13:38:55 -0500

    Envío de mensajes al correo real de los usuarios

--- main-app/compartido/mensajes-enviar.php
+++ main-app/compartido/mensajes-enviar.php
@@ -1,6 +1,13 @@
 <?php
 include("session-compartida.php");
 
+$input = json_decode(file_get_contents("php://input"), true);
+$response = [];
+
+if (!empty($input)) {
+    $_GET = $input;
+}
+
 Modulos::validarAccesoDirectoPaginas();
 $idPaginaInterna = 'CM0040';
 
@@ -16,64 +23,45 @@ $usuariosClase = new UsuariosFunciones;
 try {
     $remitente = UsuariosPadre::sesionUsuario($_SESSION["id"]);
 } catch (Exception $e) {
+    $response['status'] = false;
     include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
 }
 
-$cont = count($_POST["para"]);
-$i = 0;
 
-while ($i < $cont) {
+try {
+    $destinatario = UsuariosPadre::sesionUsuario($_GET["receptor"]);
+} catch (Exception $e) {
+    $response['status'] = false;
+    include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
+}
 
-    try {
-        $destinatario = UsuariosPadre::sesionUsuario($_POST["para"][$i]);
-    } catch (Exception $e) {
-        include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
-    }
-
-    $datos = [
-        'ema_de'             => $_SESSION["id"],
-        'ema_para'           => $_POST["para"][$i],
-        'ema_asunto'         => mysqli_real_escape_string($conexion,$_POST["asunto"]),
-        'ema_contenido'      => mysqli_real_escape_string($conexion,$_POST["contenido"]),
-        'ema_fecha'          => date("Y-m-d h:i:s"),
-        'ema_visto'          => 0,
-        'ema_eliminado_de'   => 0,
-        'ema_eliminado_para' => 0,
-        'ema_institucion'    => $config['conf_id_institucion'],
-        'ema_year'           => $_SESSION["bd"]
-    ];
-
-    try {
-        Comunicativo_Social_Email::Insert($datos, BD_ADMIN);
-    } catch (Exception $e) {
-        include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
-    }
-
-    $i++;
-
+try {
     /* 
     / Icolven compró la opción de envíar mensajes al correo de los usuarios.
     / Cuando el usuario que envía es de Icolven y no es un estudiante entonces
     / se envía dicho correo. O también cuando el mensaje es para SINTIA ADMIN.
     */
     if (
-        ($config['conf_id_institucion'] == ICOLVEN && $datosUsuarioActual['uss_tipo'] != TIPO_ESTUDIANTE)
-        || $_POST["para"][$i] == Administrativo_Usuario_Usuario::USUARIO_DEFAULT_SINTIA
+        (
+            ($config['conf_id_institucion'] == ICOLVEN || $config['conf_id_institucion'] == DEVELOPER) 
+            && $datosUsuarioActual['uss_tipo'] != TIPO_ESTUDIANTE
+        )
+        || $_GET["receptor"] == Administrativo_Usuario_Usuario::USUARIO_DEFAULT_SINTIA
     ) {
         //INICIO ENVÍO DE MENSAJE
-        $tituloMsj    = $_POST["asunto"];
+        $tituloMsj    = $_GET["asunto"];
         $contenidoMsj = '
             <p style="color:navy;">
             Hola ' . strtoupper($destinatario['uss_nombre']) . ', has recibido un mensaje a través de la plataforma SINTIA.<br>
             <b>Remitente:</b> ' . strtoupper($remitente['uss_nombre']) . '.
             </p>
 
-            <p>' . $_POST["contenido"] . '</p>
+            <p>' . $_GET["contenido"] . '</p>
         ';
 
         //Consulta de datos para el usuario al cual se enviará el correo electrónico
         $predicadoUsuario = [
-            'uss_id'      => $_POST["para"][$i],
+            'uss_id'      => $_GET["receptor"],
             'institucion' => $config['conf_id_institucion'],
             'year'        => $_SESSION["bd"]
         ];
@@ -84,7 +72,9 @@ while ($i < $cont) {
         $data = [
             'contenido_msj'  => $contenidoMsj,
             'usuario_email'  => $datosUsuario['uss_email'],
-            'usuario_nombre' => $datosUsuario['uss_nombre']
+            'usuario_nombre' => $datosUsuario['uss_nombre'],
+            'institucion_id' => $config['conf_id_institucion'],
+            'usuario_id'     => $_GET["receptor"]
         ];
 
         $asunto            = $tituloMsj;
@@ -92,10 +82,11 @@ while ($i < $cont) {
         
         EnviarEmail::enviar($data, $asunto, $bodyTemplateRoute, null, null);
     }
+
+    $response['status'] = true;
+} catch (Exception $e) {
+    $response['status'] = false;
 }
 
-$url = $usuariosClase->verificarTipoUsuario($datosUsuarioActual['uss_tipo'],'mensajes.php');
-
-include(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
-echo '<script type="text/javascript">window.location.href="' . $url . '";</script>';
+echo json_encode($response);
 exit();
