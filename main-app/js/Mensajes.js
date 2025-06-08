6d4b86b3db0d403b7e82525d55682b6e56220cdf
El gran Sherman <jhooderman@gmail.com>
2025-06-08 13:36:51 -0500

    Obtenemos los datos del formulario que envia el mensaje

--- main-app/js/Mensajes.js
+++ main-app/js/Mensajes.js
@@ -6,6 +6,21 @@
  * @param {string} nombreEmisor - Nombre del Emisor del mensaje.
  */
 function enviarMensajes(year, institucion, emisor, nombreEmisor) {
+    //Obtenemos los datos del formulario que envia el mensaje
+    const formulario = document.getElementById('formularioEnviarMensajes');
+    const botonEnviar = document.getElementById('btnEnviarMensaje'); // Obtener el botón por su ID
+
+    // Deshabilitar todos los elementos de entrada dentro del formulario
+    for (let i = 0; i < formulario.elements.length; i++) {
+        formulario.elements[i].disabled = true;
+    }
+
+    // Deshabilitar explícitamente el botón de envío.
+    if (botonEnviar) {
+        botonEnviar.disabled = true;
+        botonEnviar.textContent = 'Enviando...';
+    }
+
     // Obtener el elemento del select de usuarios
     var selectUsuario = document.getElementById('select_usuario');
     
@@ -15,6 +30,9 @@ function enviarMensajes(year, institucion, emisor, nombreEmisor) {
     
     // Almacenar los receptores seleccionados en el select múltiple
     var receptores = [];
+
+    //Guardamos en el localStorage la cantidad de destinatarios
+    localStorage.setItem("cantidadDestinatarios", selectUsuario.options.length);
 
     // Iterar sobre todas las opciones del select
     for (var i = 0; i < selectUsuario.options.length; i++) {
@@ -26,8 +44,6 @@ function enviarMensajes(year, institucion, emisor, nombreEmisor) {
             receptores.push(option.value);
         }
     }
-
-    console.log(nombreEmisor);
 
     // Enviar un mensaje para cada receptor
     receptores.forEach(function (receptor) {
