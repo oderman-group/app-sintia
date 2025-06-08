2324e098353c9e306540c63cacdae9dddf26a0d7
Jorge Diaz <ing.jorgediaz@outlook.com>
2025-06-07 20:42:51 -0500

    Se agregan comentarios para las funciones creadas y se especifica mejor el nombre de las variables y parametros

--- main-app/js/tipos-notificaciones-suscribir.js
+++ main-app/js/tipos-notificaciones-suscribir.js
@@ -1,13 +1,19 @@
 let suscribeChecked;
 let suscribeInput;
+
 const txtId = document.getElementById('txtId').value;
 const txtIdInstitucion = document.getElementById('txtIdInstitucion').value;
 const txtIdBd = document.getElementById('txtIdBd').value; 
 
-function actualizarSuscriptor(suscribir){
-    suscribeInput = suscribir;
+/**
+ * Pregunta si se quiere cambiar el estado de suscripción de un usuario a una notificación.
+ * 
+ * @param suscribir - Input (CHECKBOX) de entrada que representa la suscripción del usuario.
+ */
+function actualizarSuscriptor(inputCheckbox) {
+    suscribeInput = inputCheckbox;
     suscribeChecked = $(suscribeInput).is(':checked');
-    let idUsuario = $(suscribeInput).attr('id').split("_");
+    let idUsuario = $(suscribeInput).attr('id').split("_")[0];
     
     sweetConfirmacion(
         'Alerta!',
@@ -18,9 +24,9 @@ function actualizarSuscriptor(suscribir){
         null,
         'POST',
         { 
-            suscribirtodos : false,
+            suscribirTodos : false,
             idNotificacion: txtId,
-            idUsuario: idUsuario[0],
+            idUsuario: idUsuario,
             suscribir: suscribeChecked,
             year: txtIdBd,
             idInstitucion: txtIdInstitucion,
@@ -30,9 +36,13 @@ function actualizarSuscriptor(suscribir){
 
 }
 
+/**
+ * Pregunta si se quiere suscribir o desuscribir a todos los usuarios a una notificación.
+ * 
+ * @param checked  - El estado del checkbox que indica si se deben suscribir o desuscribir todos los usuarios.
+ */
 function actualizarSuscriptorTodos(checked){
-    suscribeChecked = checked;
-    
+    suscribeChecked = checked;    
     
     sweetConfirmacion(
         'Alerta!',
@@ -55,28 +65,34 @@ function actualizarSuscriptorTodos(checked){
 
 }
 
-function respuestaSuscripcion(result,data, respuetaSweet) {
+/**
+ * captura la respuesta de la solicitud AJAX.
+ * 
+ * @param respuestaAjax - El resultado de la solicitud AJAX.
+ * @param datosEnviados - Los datos enviados en la solicitud.
+ * @param respuetaSweet - Indica la respuesta del usuario en SweetAlert (SI o NO).
+ *
+ */
+function respuestaSuscripcion(respuestaAjax, datosEnviados, respuetaSweet) {
 
     if (!respuetaSweet) {
-        if(!data["suscribirTodos"]){
+        if(!datosEnviados["suscribirTodos"]){
             $(suscribeInput).prop('checked', !suscribeChecked);	
         }
-
                 
     }else{
-        let  resultado = result["data"];
         $.toast({
                 heading: 'Acción realizada',
-                text: resultado["msg"],
+                text: respuestaAjax["data"]["msg"],
                 position: 'bottom-right',
                 showHideTransition: 'slide',
                 loaderBg: '#26c281',
-                icon: resultado["ok"] ? 'success' : 'error',
+                icon: respuestaAjax["data"]["ok"] ? 'success' : 'error',
                 hideAfter: 5000,
                 stack: 6
         });
 
-        if(data["suscribirTodos"]){
+        if(datosEnviados["suscribirTodos"]){
             const table = $('#example1').DataTable();
             const selectedCheckboxes = table.rows().nodes().to$().find('input[name="suscrito"]');
             selectedCheckboxes.each(function () {
