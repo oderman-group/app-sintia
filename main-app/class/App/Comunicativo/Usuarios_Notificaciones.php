75d63822e4f114e2fd41127a7bbcfd9e3c3782e2
Jorge Diaz <ing.jorgediaz@outlook.com>
2025-06-07 22:01:32 -0500

    se documenta la funcion y se utiliza el metodo selectjoin

--- main-app/class/App/Comunicativo/Usuarios_Notificaciones.php
+++ main-app/class/App/Comunicativo/Usuarios_Notificaciones.php
@@ -1,7 +1,7 @@
 <?php
 require_once ROOT_PATH.'/main-app/class/Tables/BDT_tablas.php';
 
-class Comunicativo_Usuarios_Notificaciones extends BDT_Tablas {
+class Comunicativo_Usuarios_Notificaciones extends BDT_Tablas implements BDT_JoinImplements{
     public static $schema = BD_GENERAL;
 
     public static $tableName = 'usuarios_notificaciones';
@@ -10,22 +10,43 @@ class Comunicativo_Usuarios_Notificaciones extends BDT_Tablas {
 
     public  static $tableAs = 'upn';
 
+    use BDT_Join;
+
     CONST TIPO_NOTIFICACION_DESBLOQUEO_USUARIO = 1;
 
-    public static function ObtenerUsuariosDirectivosSuscripcion($year,$institucion)
+    /**
+     * Obtiene los usuarios directivos y si tienen suscripción a las notificaciones de desbloqueo de usuario.
+     * 
+     * @param int $anno Año a consultar.
+     * @param int $institucion ID de la institución a consultar.
+     * @return array Lista de usuarios directivos con su información y estado de suscripción a las notificaciones.
+     * 
+     */
+    public static function ObtenerUsuariosDirectivosSuscripcion($anno,$institucion)
     {  
-        $sql = 'SELECT 
-                us.uss_id,us.uss_usuario,us.uss_email,us.uss_nombre, us.uss_bloqueado,us.uss_estado, IFNULL(un.upn_id, 0) upn_id
-                FROM mobiliar_general.usuarios us
-                LEFT JOIN mobiliar_general.usuarios_notificaciones un ON 
-                    un.upn_tipo_notificacion = 1 AND 
-                    un.upn_usuario = us.uss_id AND 
-                    un.institucion = us.institucion AND 
-                    un.`year` = us.`year`
-                WHERE us.uss_tipo = 5 AND us.institucion = '.$institucion.' AND us.`year` = '.$year.';
-            ';
 
-        return self::ejecutarSQL($sql);
+        self::foreignKey(self::LEFT, [
+            'upn_tipo_notificacion'  => self::TIPO_NOTIFICACION_DESBLOQUEO_USUARIO,
+            'upn_usuario'            => Administrativo_Usuario_Usuario::$tableAs.'.uss_id',
+            'year'                   => Administrativo_Usuario_Usuario::$tableAs.'.year',
+            'institucion'            => Administrativo_Usuario_Usuario::$tableAs.'.institucion'
+        ]);
+
+        $predicadoJoin = [
+            Administrativo_Usuario_Usuario::$tableAs.'.uss_tipo'     => TIPO_DIRECTIVO,
+            Administrativo_Usuario_Usuario::$tableAs.'.year'         => $anno,
+            Administrativo_Usuario_Usuario::$tableAs.'.institucion'  => $institucion,
+        ];
+
+        $camposJoin = Administrativo_Usuario_Usuario::$tableAs.'.uss_id, '.
+                        Administrativo_Usuario_Usuario::$tableAs.'.uss_usuario, '.
+                        Administrativo_Usuario_Usuario::$tableAs . '.uss_email, '.
+                        Administrativo_Usuario_Usuario::$tableAs.'.uss_nombre, '.
+                        Administrativo_Usuario_Usuario::$tableAs.'.uss_bloqueado, '.
+                        Administrativo_Usuario_Usuario::$tableAs.'.uss_estado, '.
+                        'IFNULL('.self::$tableAs.'.upn_id, 0) upn_id';
+
+        return self::SelectJoin($predicadoJoin, $camposJoin, Administrativo_Usuario_Usuario::class, [self::class]);
 
     }
 
