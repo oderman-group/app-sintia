d2ece2b43d7437c777ceeab29bfd79d5cd12b21c
El gran Sherman <jhooderman@gmail.com>
2025-06-09 09:28:55 -0500

    Actualizar código de la pagina interno

--- main-app/directivo/tipos-notificaciones-suscribir-actualizar.php
+++ main-app/directivo/tipos-notificaciones-suscribir-actualizar.php
@@ -1,7 +1,7 @@
 <?php 
 include("session.php");
 Modulos::validarAccesoDirectoPaginas();
-$idPaginaInterna = 'DT0350';
+$idPaginaInterna = 'DT0351';
 
 if(!Modulos::validarSubRol([$idPaginaInterna])){
 	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
