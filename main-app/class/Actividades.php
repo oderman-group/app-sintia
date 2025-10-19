6f05d11216a862feccbbbd8adfb90cd52aa5a2ee
El gran Sherman <jhooderman@gmail.com>
2025-10-19 09:45:52 -0500

    Actualización de actividades correctamente

--- main-app/class/Actividades.php
+++ main-app/class/Actividades.php
@@ -816,7 +816,8 @@ class Actividades {
     ){
         $year= !empty($yearBd) ? $yearBd : $_SESSION["bd"];
 
-        $sql = "UPDATE ".BD_ACADEMICA.".academico_actividades SET act_registrada=1, act_fecha_registro=now() WHERE act_id=? AND institucion=? AND year=?";
+        $sql = "UPDATE ".BD_ACADEMICA.".academico_actividades SET act_registrada=1, act_fecha_registro=now() 
+        WHERE act_id=? AND institucion=? AND year=? AND (act_registrada IS NULL OR act_registrada = '' OR act_registrada = 0)";
 
         $parametros = [$idActividad, $config['conf_id_institucion'], $year];
         
