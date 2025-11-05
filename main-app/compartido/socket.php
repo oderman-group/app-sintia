0d174f9a38d83c2c97f40d52c6f91e72991d278a
El gran Sherman <jhooderman@gmail.com>
2025-11-05 11:09:10 -0500

    comentar el llamado socket

--- main-app/compartido/socket.php
+++ main-app/compartido/socket.php
@@ -1,4 +1,4 @@
-<script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
 <script>
 	var socket = io('<?= URL_API ?>', {
 		transports: ['websocket', 'polling', 'flashsocket']
@@ -13,4 +13,4 @@
 		console.log(body);
 		abrirModal("Nueva Noticia", "../compartido/noticia-modal.php", body);
 	});
-</script>
+</script> -->
