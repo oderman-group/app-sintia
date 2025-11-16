0b901a52f6125b77384cebc1d99f5c9c973c0781
Jhon Mejia Lenovo <info@oderman-group.com>
2025-11-16 06:30:19 -0500

    Optimización en paginas login y configuración

--- main-app/directivo/configuracion-sistema.php
+++ main-app/directivo/configuracion-sistema.php
@@ -12,6 +12,7 @@ if(!Modulos::validarSubRol([$idPaginaInterna])){
 require_once(ROOT_PATH."/main-app/class/categoriasNotas.php");
 require_once(ROOT_PATH."/main-app/class/Tables/BDT_configuracion.php");
 require_once ROOT_PATH.'/main-app/class/App/Academico/Calificacion.php';
+require_once(ROOT_PATH."/main-app/class/BindSQL.php");
 
 $year = $_SESSION["bd"];
 if (!empty($_GET['year'])) {
@@ -24,14 +25,17 @@ if (!empty($_GET['id'])) {
 }
 
 try {
-    $consultaConfiguracion = mysqli_query($conexion, "SELECT configuracion.*, ins_siglas, ins_years FROM " . $baseDatosServicios . ".configuracion 
-    INNER JOIN " . $baseDatosServicios . ".instituciones ON ins_id=conf_id_institucion
-    WHERE conf_id_institucion='" . $id . "' AND conf_agno='" . $year . "'");
+	$sqlConfig = "SELECT configuracion.*, ins_siglas, ins_years 
+		FROM {$baseDatosServicios}.configuracion 
+		INNER JOIN {$baseDatosServicios}.instituciones ON ins_id = conf_id_institucion
+		WHERE conf_id_institucion = ? AND conf_agno = ?";
+
+	$consultaConfiguracion = BindSQL::prepararSQL($sqlConfig, [$id, $year]);
+	$datosConfiguracion = $consultaConfiguracion ? mysqli_fetch_array($consultaConfiguracion, MYSQLI_BOTH) : [];
 } catch (Exception $e) {
     include("../compartido/error-catch-to-report.php");
+	$datosConfiguracion = [];
 }
-
-$datosConfiguracion = mysqli_fetch_array($consultaConfiguracion, MYSQLI_BOTH);
 
 $disabledPermiso = "";
 
